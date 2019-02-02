11f2ddb900af1dce64798054e9e6f8775c3b48e6
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2019-02-02 13:23:32 +0100

    Add newlines after MARKs in RedirectionCoordinator to increase consistency

--- XCoordinator/Classes/RedirectionCoordinator.swift
+++ XCoordinator/Classes/RedirectionCoordinator.swift
@@ -21,6 +21,7 @@
 open class RedirectionCoordinator<RouteType: Route, TransitionType: TransitionProtocol>: Coordinator {
 
     // MARK: - Stored properties
+
     private let superTransitionPerformer: AnyTransitionPerformer<TransitionType>
     private let viewControllerBox = ReferenceBox<UIViewController>()
     private let _prepareTransition: ((RouteType) -> TransitionType)?
@@ -71,6 +72,7 @@ open class RedirectionCoordinator<RouteType: Route, TransitionType: TransitionPr
     }
 
     // MARK: - Methods
+
     open func presented(from presentable: Presentable?) {
         viewController?.presented(from: presentable)
         viewControllerBox.releaseStrongReference()
@@ -91,6 +93,7 @@ open class RedirectionCoordinator<RouteType: Route, TransitionType: TransitionPr
 }
 
 // MARK: - Deprecated
+
 extension RedirectionCoordinator {
 
     ///
